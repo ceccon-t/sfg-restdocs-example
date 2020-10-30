33f694003d75dd2ff5d20207a91fd55ce71f15a7
Tiago Ceccon <ceccon.t@hotmail.com>
2020-10-29 23:45:54 -0300

    Serve documentation

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-29T23:37:15-0300",
+    date = "2020-10-29T23:44:23-0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Ubuntu)"
 )
 @Component
